3c9a3085eedac3a6e544402384884b18f1d52633
Doyo Abdikadir <doyo.doyo@student.moringaschool.com>
2022-06-18 13:16:07 +0300

    ft: Neighbourhood model created

--- app/models.py
+++ app/models.py
@@ -28,3 +28,29 @@ class Profile(models.Model):
         return cls.objects.filter(user__username__icontains=name).all()
 
 
+class Neighbourhood(models.Model):
+    name = models.CharField(max_length=50)
+    location = models.CharField(max_length=60)
+    user = models.ForeignKey(Profile, on_delete=models.CASCADE, related_name='hood')
+    logo = models.ImageField(upload_to='images/',default='default.png')
+    description = models.TextField()
+    health_tell = models.IntegerField(null=True, blank=True)
+    occupants = models.IntegerField(null=True, blank=True)
+    police_number = models.IntegerField(null=True, blank=True)
+
+    def __str__(self):
+        return f'{self.name} hood'
+
+    def create_neighborhood(self):
+        self.save()
+
+    def delete_neighborhood(self):
+        self.delete()
+
+    @classmethod
+    def find_neighborhood(cls, neighborhood_id):
+        return cls.objects.filter(id=neighborhood_id)
+    
+    @classmethod
+    def update_occupants(cls, occupants, value):
+        cls.objects.filter(occupants=occupants).update(occupants=value)
