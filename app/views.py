daaa373785243a5e9aa4a7bf53a6548d225cdbb0
Doyo Abdikadir <doyo.doyo@student.moringaschool.com>
2022-06-19 13:02:27 +0300

    ft: view function to create neighbourhood created

--- app/views.py
+++ app/views.py
@@ -1,9 +1,23 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
 from django.contrib.auth.decorators import login_required
 from .models import Neighbourhood
+from .forms import NeighbourHoodForm
 
 # Create your views here.
 @login_required(login_url='/accounts/login')
 def index(request):
     all_hoods = Neighbourhood.objects.all()
     return render(request, 'index.html',{'all_hoods': all_hoods,})
+
+
+def create_hood(request):
+    if request.method == 'POST':
+        form = NeighbourHoodForm(request.POST, request.FILES)
+        if form.is_valid():
+            hood = form.save(commit=False)
+            hood.admin = request.user.profile
+            hood.save()
+            return redirect('ndex')
+    else:
+        form = NeighbourHoodForm()
+    return render(request, 'newhood.html', {'form': form})
