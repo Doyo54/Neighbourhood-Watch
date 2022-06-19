127f9a3413abc5aa46e002027c4a9347dda09e03
Doyo Abdikadir <doyo.doyo@student.moringaschool.com>
2022-06-19 13:05:36 +0300

    ft: html content to display all hoods create

--- app/views.py
+++ app/views.py
@@ -17,7 +17,7 @@ def create_hood(request):
             hood = form.save(commit=False)
             hood.admin = request.user.profile
             hood.save()
-            return redirect('ndex')
+            return redirect('index')
     else:
         form = NeighbourHoodForm()
     return render(request, 'newhood.html', {'form': form})
