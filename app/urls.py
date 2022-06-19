eef464bec13736edca3b52aad01a26dc4dfb667c
Doyo Abdikadir <doyo.doyo@student.moringaschool.com>
2022-06-19 13:09:48 +0300

    ft: urls for adding,joining and leaving a neighbouhod created

--- app/urls.py
+++ app/urls.py
@@ -7,6 +7,9 @@ from . import views
 urlpatterns = [
     
     re_path(r'^$',views.index,name='index'),
+    re_path(r'^add-hood/', views.create_hood, name='new-hood'),
+    re_path(r'^join_hood/(?P<id>\w+)/', views.join_hood, name='join-hood'),
+    re_path(r'^leave_hood/(?P<id>\w+)/', views.leave_hood, name='leave-hood'),
 ]   
 if settings.DEBUG:
     urlpatterns+= static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
