6e638cdcf00e4430c3f62a280d6cfc91ffd81fb5
Doyo Abdikadir <doyo.doyo@student.moringaschool.com>
2022-06-19 14:47:33 +0300

    ft: update user profile form created

--- app/forms.py
+++ app/forms.py
@@ -1,8 +1,13 @@
 from django import forms
 from django.contrib.auth.models import User
-from .models import Neighbourhood
+from .models import Neighbourhood,Profile
 
 class NeighbourHoodForm(forms.ModelForm):
     class Meta:
         model = Neighbourhood
         exclude = ('user',)
+
+class UpdateUserProfileForm(forms.ModelForm):
+    class Meta:
+        model = Profile
+        fields = ['name', 'profile_picture', 'location', 'bio','neighbourhood']
