9aecf7b5dce0b4ca069e9c73aa3b5b8e1ed800c9
Doyo Abdikadir <doyo.doyo@student.moringaschool.com>
2022-06-19 12:59:40 +0300

    ft: view function to view neighbourhoods created

--- app/views.py
+++ app/views.py
@@ -1,7 +1,9 @@
 from django.shortcuts import render
 from django.contrib.auth.decorators import login_required
+from .models import Neighbourhood
 
 # Create your views here.
 @login_required(login_url='/accounts/login')
 def index(request):
-    return render(request, 'index.html')
+    all_hoods = Neighbourhood.objects.all()
+    return render(request, 'index.html',{'all_hoods': all_hoods,})
