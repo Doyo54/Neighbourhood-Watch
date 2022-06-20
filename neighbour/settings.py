1232b603340b616ae9ade5de77f43ac75e971f83
Kemunto Cindy <cindy.kemu@student.moringaschool.com>
2022-06-20 08:31:04 +0300

    Dev: make changes to the db

--- neighbour/settings.py
+++ neighbour/settings.py
@@ -13,6 +13,7 @@ https://docs.djangoproject.com/en/4.0/ref/settings/
 from pathlib import Path
 import os
 
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -39,7 +40,8 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'app',
-    'bootstrap3',
+    'bootstrap4',
+    'business',
 ]
 
 MIDDLEWARE = [
@@ -76,16 +78,22 @@ WSGI_APPLICATION = 'neighbour.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.0/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#        'ENGINE': 'django.db.backends.postgresql',
+#         'NAME': 'neighbour',
+#         'USER': 'doyo',
+#     'PASSWORD':'doyo123',
+#     }
+# }
+
+# if (os.environ.get('DJANGO_DEV') == 'True'):
 DATABASES = {
-    'default': {
-       'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'neighbour',
-        'USER': 'doyo',
-    'PASSWORD':'doyo123',
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        }
     }
-}
-
-
 # Password validation
 # https://docs.djangoproject.com/en/4.0/ref/settings/#auth-password-validators
 
